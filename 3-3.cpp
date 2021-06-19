3b2d9d29abc4ab4203eec7cc7b734d9711965732
godhye <79397487+godhye@users.noreply.github.com>
2021-06-19 14:41:27 +0900

    숫자카드게임

--- 3-3.cpp
+++ 3-3.cpp
@@ -14,11 +14,14 @@ int main()
 	vector<int> vecnmin;
 
 	cin >> N >> M;
-
+	int result = 0 ;
 	for (int i = 0; i < N; i++)
 	{
 		int n;
-		int n_min = 10000;
+		//int n_min = 10000;
+		int n_min = 10001; //왜10001 이지?,,,,10000으로해도 최소값은 구해지지않나????
+
+		
 		for (int k = 0; k < M; k++)
 		{
 			
@@ -29,12 +32,14 @@ int main()
 
 		}
 
-		vecnmin.push_back(n_min);
+		//vecnmin.push_back(n_min); 굳이 값들을 기억할 필요 없고 작은 값만 찾으면 되니깐 벡터 사용x
+		result = max(result, n_min);
 	}
 
-	sort(vecnmin.begin(), vecnmin.end());
+	//sort(vecnmin.begin(), vecnmin.end());
 
-	printf("%d", vecnmin[N - 1]);
+	//printf("%d", vecnmin[N - 1]);
+	printf("%d", result);
 
 	return 0;
 }

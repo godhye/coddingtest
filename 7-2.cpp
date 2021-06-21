5c316a1a32a0c68c0e4a033b35743867178198a1
godhye <79397487+godhye@users.noreply.github.com>
2021-06-21 21:22:19 +0900

    이진탐색 재귀

---
+++ 7-2.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+
+ 
+using namespace std;
+int N, K;
+int cnt = 0;
+//이진탐색
+bool binarysearch(vector<int> list, int start, int end)
+{
+	int middle = (start + end) / 2;
+	cnt++;
+
+	if (list[middle] == K)
+		return cnt;
+
+	else if (list[middle] > K)
+		binarysearch(list, start, middle - 1);
+	else
+		binarysearch(list,  middle+1 , end);
+}
+
+
+int main()
+{
+ 
+
+	cin >> N >> K;
+
+	vector <int> list;
+	for (int i = 0; i < N; i++)
+	{
+		int c;
+		cin >> c;
+		list.push_back(c);
+	}
+
+	binarysearch(list, 0, list.size() - 1);
+
+	cout << cnt << endl;
+	return 1;
+
+}

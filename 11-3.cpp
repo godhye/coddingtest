fdf6d732ef8320f7517a831f7291467afd9c3b81
godhye <79397487+godhye@users.noreply.github.com>
2021-07-01 23:54:23 +0900

    문자열뒤집기
    첫번째 판단 넣어주고 다음것들은 현재원소랑 다음원소랑 다르지만 확인

--- 11-3.cpp
+++ 11-3.cpp
@@ -4,22 +4,30 @@ using namespace std;
 
 int main(){
     string strn;
-    cin>>strn;
-    int result0 =0;
-    int result1 =0;
+	cin >> strn;
+	int result0 = 0; //0뒤집기
+	int result1 = 0; //1뒤집기
     
- 
-    for(int i =0; i<strn.length()-1;i++)
-    {
-        //1뒤집기
-        if((int(strn[i])-48 != 0 )&& int(strn[i+1])-48 ==0)
-            result0++;
-         //0뒤집기
-        if(int(strn[i])-48 != 1 && int(strn[i])-48 == 1)
+    //첫번째 문자열에 대해서 처리하기
+    if(strn[0] -48  == 0)
         result1++;
-        
-    }
- 
-    cout <<min(result0 , result1);
-    return 0;
+    else
+       result0++; 
+    
+	for (int i = 0; i<strn.length() - 1; i++)
+	{
+		//1뒤집기
+		if (int(strn[i]) != int(strn[i + 1]) )
+            {
+                if((strn[i+1] -48) == 1)
+                    result0++;
+                else
+                    result1++;
+                
+            }
+
+	}
+
+	cout << min(result0, result1);
+	return 0;
 }

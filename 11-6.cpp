c03cee571e0fd0805697c2dab9ae83f393222965
godhye <79397487+godhye@users.noreply.github.com>
2021-07-04 19:02:36 +0900

    무지의 라이브 먹방
    sort , compare 함수 쓰는법 몰랏고 , 순서있는 일 우선순위 큐에 넣어서 처리하는거

--- 11-6.cpp
+++ 11-6.cpp
@@ -1,41 +1,43 @@
 #include <string>
 #include <vector>
-#include <iostream>
-using namespace std;
+#include <queue>
+#include <algorithm>
 
- 
+using namespace std;
+bool compare(pair<int,int> a, pair<int,int> b){
+    return a.second < b.second;
+}
+
 int solution(vector<int> food_times, long long k) {
-    int answer = 0;
-    int index =0;
-    while(k >0)
-    {
-        for(int i =0 ; i < food_times.size();i++, index++)
-        {
-            if(food_times[index%3] > 0)
-            {
-                food_times[index%3]--;
-                index++;
-                break;
-            }
-            
-            if(food_times[index%3] == 0)
-                continue;
-            
-          
-        }
-        
-        k--;
-        if(isfinished(food_times) == true)
-               return -1;
+    long long sum=0;
+    for(int i=0;i<food_times.size();i++){
+        sum+=food_times[i];
     }
-    
-    
-       for(int i =0 ; i < food_times.size(); i++, index++)
-        {
-            if(food_times[index%3] > 0)
-            {
-                return (index%3 +1 );
-            }
-         }
-    
+    if(sum<=k) return -1;
+
+    priority_queue<pair<int,int>> pq;
+    for(int i=0;i<food_times.size();i++){
+        pq.push({-food_times[i],i+1}); // 최소힙
+    }
+
+    sum=0;
+    long long previous=0;
+    long long length=food_times.size();
+    while(sum+((-pq.top().first - previous)*length)<=k){
+        int now=-pq.top().first;
+        pq.pop(); //다먹음
+        sum+=(now-previous)*length;
+        length-=1;
+        previous=now;
+    }
+    vector<pair<int,int>> result;
+    while(!pq.empty()){
+        int food_time=-pq.top().first;
+        int num=pq.top().second;
+        pq.pop();
+        result.push_back({food_time,num});
+    }
+    sort(result.begin(),result.end(),compare);
+    return result[(k-sum)%length].second;
+
 }
